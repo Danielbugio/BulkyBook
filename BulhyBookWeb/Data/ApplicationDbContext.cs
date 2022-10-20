b7626f6344fa2fe8eb0be8138489c61e8d536e6a
Daniel Domingues <danielbugio@hotmail.com>
2022-10-20 12:15:55 +0100

    Add project files.

---
+++ BulhyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BulhyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulhyBookWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+
+    }
+}
---
+++ BulhyBookWeb/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulhyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        // Ao igualar a propriedade em dateTime.Now faz com que sempre que se instancie um objeto desta classe este valor fica preenchida com a data atual
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
