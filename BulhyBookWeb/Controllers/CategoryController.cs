16c1f2ce228b4eb0c8276d04e1555fd185717c2b
Daniel Domingues <danielbugio@hotmail.com>
2022-10-21 16:15:29 +0100

    21/10

--- BulhyBookWeb/Controllers/CategoryController.cs
+++ BulhyBookWeb/Controllers/CategoryController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulhyBookWeb.Data;
+using BulhyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulhyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+        //GET
+
+        public IActionResult Create()
+        {
+            
             return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
